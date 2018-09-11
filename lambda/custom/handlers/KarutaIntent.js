ae416055015af0b98e5939c52d38705a4c3bf048
hideokamoto <hide.okamoto@digitalcube.jp>
2018-09-11 20:11:33 +0900

    update ssml

--- lambda/custom/handlers/KarutaIntent.js
+++ lambda/custom/handlers/KarutaIntent.js
@@ -12,14 +12,21 @@ const KarutaIntenttHandler = {
       'カルタゲームを始めますか？',
       'カルタゲームを始めますか？ルールについて確認しますか？'
     ]
-    const speech = `カルタを始めます。問題、${quiz.description}。<break time="5s"/>正解は${quiz.name}です。`
+    const speech = [
+      '<p>カルタを始めます。</p>',
+      `<p>問題、${quiz.description}</p>`,
+      '<p>このサービスはなんでしょう？</p>',
+      "<audio src='https://s3.amazonaws.com/ask-soundlibrary/foley/amzn_sfx_clock_ticking_long_01.mp3'/>",
+      "<audio src='https://s3.amazonaws.com/ask-soundlibrary/musical/amzn_sfx_bell_timer_01.mp3'/>",
+      `<p>時間となりました。正解は<break time="0.3s" />${quiz.name_kana}です。</p>`
+    ]
     handlerInput.attributesManager.setSessionAttributes({
         state: STATES.start,
         answer: quiz.name,
         quiz: quiz.description
     })
     return handlerInput.responseBuilder
-      .speak(speech)
+      .speak(speech.join(''))
       .withSimpleCard(`${SKILL_NAME}： 問題`, quiz.description)
       .getResponse()
   }
